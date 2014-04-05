14e90d709e22effa8fca83f59b989418d40e52db
Christophe Hoch <christophe.hoch@gmail.com>
2014-04-05 12:27:43 +0200

    Fix typo in Unit creation on upper lane

--- public/js/Unit.js
+++ public/js/Unit.js
@@ -1,7 +1,15 @@
-function Unit(player, type) {
+function Unit(scene, player, type) {
     console.log('Player ' + player + ' is creating a ' + type)
     
+    this.scene = scene;
     
+    this.cube = new THREE.Mesh( new THREE.CubeGeometry(0.9,0.05,0.9),  new THREE.MeshBasicMaterial( { color: 0x333333 } ) )
+	this.cube.position.x = 2
+	this.cube.position.y = 3
+	this.cube.position.z = 2
+	this.cube.castShadow = true
+	this.cube.receiveShadow = true
+	this.scene.add(this.cube)
     
     
 }
