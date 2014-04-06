63bac97837445490adf49c85fd269a8a0e2186ae
Aurelien Defossez <defossez.aurelien@gmail.com>
2014-04-06 11:32:59 +0200

    [Units] Builders and sap carriers have attack values

--- public/js/Config.js
+++ public/js/Config.js
@@ -60,7 +60,7 @@ Game.config = {
 	units: {
 		// Porteur de sève
 		sapCarrier: {
-			attack: 0,
+			attack: 1,
 			buildingAttack: 0,
 			cooldown: 1,
 			hp: 5,
@@ -103,7 +103,7 @@ Game.config = {
 		},
 		// Constructeur
 		builder: {
-			attack: 0,
+			attack: 1,
 			buildingAttack: 0,
 			cooldown: 1,
 			hp: 5,
